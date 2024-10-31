447eabd5ae51854f9f99de6396797d701cfab8bb
Sergei12677 <gorod_26@icloud.com>
2024-10-31 20:18:03 +0500

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,13 @@
+from fake_math import divide as fake_divide
+from math import inf
+
+
+result1 = fake_divide(69,3)
+result2 = fake_divide(3,0)
+result3 = fake_divide(49,7)
+result4 = fake_divide(15,0)
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
